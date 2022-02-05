9800016a0d1787857b6edfeaf006deb3a0a6d7fb
duda-m <dudadev35@gmail.com>
2022-02-04 23:35:37 -0300

    Adding test package and Class02

---
+++ data-structure-algorithms/src/com/duda/datastructure/vector/test/Class02.java
@@ -0,0 +1,16 @@
+package com.duda.datastructure.vector.test;
+
+import com.duda.datastructure.vector.Vector;
+
+public class Class02 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Vector v = new Vector(5);
+		
+		int i = 0;
+		i++;
+	}
+
+}
