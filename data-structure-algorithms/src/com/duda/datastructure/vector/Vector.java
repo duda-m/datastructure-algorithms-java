ecf2b5def9e563ab8a825f0e74499f97985b2258
duda-m <dudadev35@gmail.com>
2022-02-05 15:36:34 -0300

    Creating Add method

--- data-structure-algorithms/src/com/duda/datastructure/vector/Vector.java
+++ data-structure-algorithms/src/com/duda/datastructure/vector/Vector.java
@@ -7,4 +7,13 @@ public class Vector {
 	public Vector(int capacity) {
 		this.elements = new String[capacity];
 	}
+	
+	public void add(String element) {
+		for(int i = 0; i<this.elements.length; i++) {
+			if(this.elements[i] == null) {
+				this.elements[i] = element;
+				break;
+			}
+		}
+	}
 }
