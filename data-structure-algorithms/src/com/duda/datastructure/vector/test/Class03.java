fb3d35caaed0ce9e6351027264ca07a0eba91ad6
duda-m <dudadev35@gmail.com>
2022-02-05 17:04:08 -0300

    Adding try catch in Class03

--- data-structure-algorithms/src/com/duda/datastructure/vector/test/Class03.java
+++ data-structure-algorithms/src/com/duda/datastructure/vector/test/Class03.java
@@ -6,9 +6,15 @@ public class Class03 {
 
 	public static void main(String[] args) {
 		
-		Vector v = new Vector(5);
+		Vector v = new Vector(2);
 		
-		v.add("first element");
+		try {
+			v.add("first element");
+			v.add("2nd element");
+			v.add("3rd element");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 		
 
 	}
