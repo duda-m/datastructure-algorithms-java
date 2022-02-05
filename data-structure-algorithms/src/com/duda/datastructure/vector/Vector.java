c916891b0e7509e5cb719bcf345b81c866780e73
duda-m <dudadev35@gmail.com>
2022-02-05 16:58:25 -0300

    Modifying add method(with size attribute)

--- data-structure-algorithms/src/com/duda/datastructure/vector/Vector.java
+++ data-structure-algorithms/src/com/duda/datastructure/vector/Vector.java
@@ -10,12 +10,22 @@ public class Vector {
 		this.size = 0;
 	}
 	
-	public void add(String element) {
+	/*public void add(String element) {
 		for(int i = 0; i<this.elements.length; i++) {
 			if(this.elements[i] == null) {
 				this.elements[i] = element;
 				break;
 			}
+		}
+	}*/
+	
+	public void add(String element) throws Exception {
+		
+		if(this.size<this.elements.length) {
+			this.elements[this.size] = element;
+			this.size++;
+		}else {
+			throw new Exception("The vector is full, this action is not possible");
 		}
 	}
 }
