392cd0318c747463024ffaaf2fdbe3e77adc60fd
duda-m <dudadev35@gmail.com>
2022-02-04 23:45:59 -0300

    little change in the Class02

--- data-structure-algorithms/src/com/duda/datastructure/vector/test/Class02.java
+++ data-structure-algorithms/src/com/duda/datastructure/vector/test/Class02.java
@@ -5,7 +5,6 @@ import com.duda.datastructure.vector.Vector;
 public class Class02 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
 		Vector v = new Vector(5);
 		
