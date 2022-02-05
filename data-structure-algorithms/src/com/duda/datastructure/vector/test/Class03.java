6ec51cbabcf1c6e09b3a568c14c043eccf5b8fe0
duda-m <dudadev35@gmail.com>
2022-02-05 16:27:47 -0300

    Creating Class03

---
+++ data-structure-algorithms/src/com/duda/datastructure/vector/test/Class03.java
@@ -0,0 +1,16 @@
+package com.duda.datastructure.vector.test;
+
+import com.duda.datastructure.vector.Vector;
+
+public class Class03 {
+
+	public static void main(String[] args) {
+		
+		Vector v = new Vector(5);
+		
+		v.add("first element");
+		
+
+	}
+
+}
