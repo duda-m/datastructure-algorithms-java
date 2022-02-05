7ed8a581db9ef3d7c21e9b7ec5b3d52338a5df7c
duda-m <dudadev35@gmail.com>
2022-02-05 17:14:12 -0300

    Including some comments

--- data-structure-algorithms/src/com/duda/datastructure/vector/Vector.java
+++ data-structure-algorithms/src/com/duda/datastructure/vector/Vector.java
@@ -3,6 +3,8 @@ package com.duda.datastructure.vector;
 public class Vector {
 
 	private String[] elements;
+	
+	//the size measures the quantity of elements in the vector, could be less than the total capacity or could be equal
 	private int size;
 	
 	public Vector(int capacity) {
@@ -20,7 +22,7 @@ public class Vector {
 	}*/
 	
 	public void add(String element) throws Exception {
-		
+		//Verifying if the length of the vector is large than the size. If yes we can add more elements. If not a message will appear.
 		if(this.size<this.elements.length) {
 			this.elements[this.size] = element;
 			this.size++;
