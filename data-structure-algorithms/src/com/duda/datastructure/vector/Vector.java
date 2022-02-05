71b8013bf29f900551994ffab58818cc564e6a7c
duda-m <dudadev35@gmail.com>
2022-02-05 16:41:09 -0300

    Creating a size attribute and implementing in the initialization method

--- data-structure-algorithms/src/com/duda/datastructure/vector/Vector.java
+++ data-structure-algorithms/src/com/duda/datastructure/vector/Vector.java
@@ -3,9 +3,11 @@ package com.duda.datastructure.vector;
 public class Vector {
 
 	private String[] elements;
+	private int size;
 	
 	public Vector(int capacity) {
 		this.elements = new String[capacity];
+		this.size = 0;
 	}
 	
 	public void add(String element) {
