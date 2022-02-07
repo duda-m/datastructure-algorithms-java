7d7c15c176fe81d8ad555bb0e9650e76e97e2cae
duda-m <dudadev35@gmail.com>
2022-02-07 16:32:04 -0300

    Creating Class11

---
+++ data-structure-algorithms/src/com/duda/datastructure/vector/test/Class11.java
@@ -0,0 +1,17 @@
+package com.duda.datastructure.vector.test;
+
+import com.duda.datastructure.vector.List;
+import com.duda.datastructure.vector.VectorObjects;
+
+public class Class11 {
+
+	public static void main(String[] args) {
+		
+		List<String> vector = new List<String>(1);
+		
+		
+		vector.add("Element");
+		System.out.println(vector);
+	}
+
+}
