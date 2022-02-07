3277bb5a00f60c8c8d8a2efa6f84775ca9d9b6dd
duda-m <dudadev35@gmail.com>
2022-02-07 19:44:08 -0300

    Creating Class13

---
+++ data-structure-algorithms/src/com/duda/datastructure/pile/test/Class13.java
@@ -0,0 +1,16 @@
+package com.duda.datastructure.pile.test;
+
+import com.duda.datastructure.pile.Pile;
+
+public class Class13 {
+
+	public static void main(String[] args) {
+		
+		Pile<Integer> p = new Pile<Integer>();
+		
+		System.out.println(p);
+		
+		System.out.println(p.size());
+	}
+
+}
