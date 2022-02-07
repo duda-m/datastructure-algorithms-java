9d4c1b1181903ce43c717485c26601b87676f208
duda-m <dudadev35@gmail.com>
2022-02-07 10:31:06 -0300

    Overloading add method to include an element in any position we want.

--- data-structure-algorithms/src/com/duda/datastructure/vector/Vector.java
+++ data-structure-algorithms/src/com/duda/datastructure/vector/Vector.java
@@ -42,6 +42,23 @@ public class Vector {
 		return false;
 	}
 	
+	public boolean add(int position, String element) {
+		
+		//checking whether the position is valid 
+		if(!(position >=0 && position < size)) {
+			throw new IllegalArgumentException("Invalid position");
+		}
+		
+		//moving the elements
+		for(int i = size-1; i >= position; i--) {
+			this.elements[i+1] = this.elements[i];
+		}
+		this.elements[position] = element;
+		this.size++;
+		
+		return true;
+	}
+	
 	public int size() {
 		return this.size;
 	}
