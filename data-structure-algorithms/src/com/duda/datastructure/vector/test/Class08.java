706529132e76673d34b6dc8a76896b9e85d0a40d
duda-m <dudadev35@gmail.com>
2022-02-07 11:27:43 -0300

    Creating Class08

---
+++ data-structure-algorithms/src/com/duda/datastructure/vector/test/Class08.java
@@ -0,0 +1,20 @@
+package com.duda.datastructure.vector.test;
+
+import com.duda.datastructure.vector.Vector;
+
+public class Class08 {
+
+	public static void main(String[] args) {
+		
+		Vector v = new Vector(2);
+
+		v.add("2");
+		v.add("3");
+		v.add("5");
+		v.add("6");
+		
+		System.out.println(v);
+		
+	}
+
+}
