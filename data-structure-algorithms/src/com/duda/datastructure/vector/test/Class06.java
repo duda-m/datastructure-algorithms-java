e957b4bffe32d344b7fe91a76661a5126f4906a3
duda-m <dudadev35@gmail.com>
2022-02-06 23:25:42 -0300

    Creating Class06

---
+++ data-structure-algorithms/src/com/duda/datastructure/vector/test/Class06.java
@@ -0,0 +1,20 @@
+package com.duda.datastructure.vector.test;
+
+import com.duda.datastructure.vector.Vector;
+
+public class Class06 {
+
+	public static void main(String[] args) {
+		
+		Vector v = new Vector(12);
+		
+		v.add("first element");
+		v.add("2nd element");
+		v.add("3rd element");
+		
+		System.out.println(v.search("2nd element"));
+		System.out.println(v.search("element one"));
+
+	}
+
+}
