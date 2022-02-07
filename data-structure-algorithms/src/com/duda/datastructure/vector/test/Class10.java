cfc879f82f5bbf7eed443bb0ff9e7205dfc96809
duda-m <dudadev35@gmail.com>
2022-02-07 15:40:52 -0300

    Implementing search method in Class10

--- data-structure-algorithms/src/com/duda/datastructure/vector/test/Class10.java
+++ data-structure-algorithms/src/com/duda/datastructure/vector/test/Class10.java
@@ -8,13 +8,27 @@ public class Class10 {
 		
 		VectorObjects vector = new VectorObjects(3);
 		
+		PersonalInformation p1 = new PersonalInformation("1person", "1234-5678","person1@email.com");
+		PersonalInformation p2 = new PersonalInformation("2person", "9000-0009","person2@email.com");
+		PersonalInformation p3 = new PersonalInformation("3person", "8000-0008","person3@email.com");
 		
-		vector.add(3);
-		vector.add(4);
-		vector.add(5);
+		//same content however different memory location
+		PersonalInformation p4 = new PersonalInformation("1person", "1234-5678","person1@email.com");
+		
+		vector.add(p1);
+		vector.add(p2);
+		vector.add(p3);
 		
 		System.out.println(vector);
 		System.out.println("Size = " + vector.size());
+		
+		int pos = vector.search(p4);
+		
+		if(pos >= 0) {
+			System.out.println("Found it!");
+		}else {
+			System.out.println("This element does not exist");
+		}
 	}
 
 }
