2cc9dbdccf815f5073eef1773a67e085618a90c8
duda-m <dudadev35@gmail.com>
2022-02-06 21:06:20 -0300

    Erasing the try-catch

--- data-structure-algorithms/src/com/duda/datastructure/vector/test/Class03.java
+++ data-structure-algorithms/src/com/duda/datastructure/vector/test/Class03.java
@@ -5,17 +5,12 @@ import com.duda.datastructure.vector.Vector;
 public class Class03 {
 
 	public static void main(String[] args) {
-		
+
 		Vector v = new Vector(2);
-		
-		try {
-			v.add("first element");
-			v.add("2nd element");
-			v.add("3rd element");
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		
+
+		v.add("first element");
+		v.add("2nd element");
+		v.add("3rd element");
 
 	}
 
