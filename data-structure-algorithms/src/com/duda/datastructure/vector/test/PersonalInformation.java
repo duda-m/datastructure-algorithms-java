dcecdc170e04ac87626d08edc75d7652753292b4
duda-m <dudadev35@gmail.com>
2022-02-07 15:47:01 -0300

    Implementing equals method in PersonalInformation

--- data-structure-algorithms/src/com/duda/datastructure/vector/test/PersonalInformation.java
+++ data-structure-algorithms/src/com/duda/datastructure/vector/test/PersonalInformation.java
@@ -40,6 +40,43 @@ public class PersonalInformation {
 	public String toString() {
 		return "PersonalInformation [name=" + name + ", number=" + number + ", email=" + email + "]";
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((email == null) ? 0 : email.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + ((number == null) ? 0 : number.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		PersonalInformation other = (PersonalInformation) obj;
+		if (email == null) {
+			if (other.email != null)
+				return false;
+		} else if (!email.equals(other.email))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (number == null) {
+			if (other.number != null)
+				return false;
+		} else if (!number.equals(other.number))
+			return false;
+		return true;
+	}
 	
 	
 }
