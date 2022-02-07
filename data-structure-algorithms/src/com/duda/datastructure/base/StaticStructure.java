412eb418ebd09d94af85a3363ccb70da9e46be03
duda-m <dudadev35@gmail.com>
2022-02-07 19:12:07 -0300

    Starting the stack part. Creating StaticStructure class

---
+++ data-structure-algorithms/src/com/duda/datastructure/base/StaticStructure.java
@@ -0,0 +1,79 @@
+package com.duda.datastructure.base;
+
+public class StaticStructure<T> {
+
+	private T[] elements;
+	private int size;
+
+	public StaticStructure(int capacity) {
+		this.elements = (T[])new Object[capacity];
+		this.size =0;
+	}
+	public StaticStructure() {
+		this(10);
+	}
+	public boolean add(T element){
+		this.increaseCapacity();
+		if(this.size<this.elements.length) {
+			this.elements[this.size] = element;
+			this.size++;
+			return true;
+		}
+		return false;
+	}
+	public boolean add(int position, T element) {
+
+		//checking whether the position is valid 
+		if(!(position >=0 && position < size)) {
+			throw new IllegalArgumentException("Invalid position");
+		}
+
+		this.increaseCapacity();
+
+		//moving the elements
+		for(int i = size-1; i >= position; i--) {
+			this.elements[i+1] = this.elements[i];
+		}
+		this.elements[position] = element;
+		this.size++;
+
+		return true;
+	}
+	public void increaseCapacity() {
+		//checking if the vector is full. 
+		if(this.size == this.elements.length) {
+			//creating a new vector with twice the size of the old one
+			T[] newElements = (T[])new Object[this.elements.length*2];
+
+			for(int i = 0; i < this.elements.length; i++) {
+				newElements[i] = this.elements[i];
+			}
+			this.elements = newElements;
+		}
+	}
+	public int size() {
+		return this.size;
+	}
+
+	@Override
+	public String toString() {
+
+		StringBuilder s = new StringBuilder();
+		s.append("[");
+
+		//covers all elements of the vector, except the last one.
+		for(int i = 0; i < this.size-1; i++) {
+			s.append(this.elements[i]);
+			s.append(", ");
+		}
+
+		//adds the last element of the vector
+		if(this.size > 0) {
+			s.append(this.elements[this.size-1]);
+		}
+
+		s.append("]");
+
+		return s.toString();
+	}
+}
