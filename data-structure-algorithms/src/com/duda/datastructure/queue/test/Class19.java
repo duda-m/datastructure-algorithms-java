c4dc6a5c6125266c7b0de0d0048f560919322c37
duda-m <dudadev35@gmail.com>
2022-02-07 21:40:53 -0300

    Creating Class19

---
+++ data-structure-algorithms/src/com/duda/datastructure/queue/test/Class19.java
@@ -0,0 +1,16 @@
+package com.duda.datastructure.queue.test;
+
+import com.duda.datastructure.queue.Queue;
+
+public class Class19 {
+
+	public static void main(String[] args) {
+		
+		Queue<Integer> q = new Queue<Integer>();
+		
+		System.out.println(q.IsEmpty());
+		System.out.println(q.size());
+
+	}
+
+}
