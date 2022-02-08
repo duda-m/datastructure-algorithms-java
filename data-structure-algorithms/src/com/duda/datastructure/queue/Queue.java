e157cdb397528cc08df25c4f9d91307bd9a27f47
duda-m <dudadev35@gmail.com>
2022-02-07 21:40:20 -0300

    Creating a class named Queue

---
+++ data-structure-algorithms/src/com/duda/datastructure/queue/Queue.java
@@ -0,0 +1,14 @@
+package com.duda.datastructure.queue;
+
+import com.duda.datastructure.base.StaticStructure;
+
+public class Queue<T> extends StaticStructure<T> {
+
+	public Queue() {
+		super();
+	}
+	public Queue(int capacity) {
+		super(capacity);
+	}
+
+}
