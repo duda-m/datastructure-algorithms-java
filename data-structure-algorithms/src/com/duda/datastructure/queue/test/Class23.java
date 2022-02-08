4a000886e73da9c5367cba30b23e2af063f06c8f
duda-m <dudadev35@gmail.com>
2022-02-08 17:49:24 -0300

    Creating Class23

---
+++ data-structure-algorithms/src/com/duda/datastructure/queue/test/Class23.java
@@ -0,0 +1,20 @@
+package com.duda.datastructure.queue.test;
+
+import com.duda.datastructure.queue.Queue;
+
+public class Class23 {
+
+	public static void main(String[] args) {
+
+		Queue<Integer> q = new Queue<>();
+
+		q.Queues(8);
+		q.Queues(13);
+		q.Queues(79);
+		q.Queues(57);
+		
+		
+
+	}
+
+}
